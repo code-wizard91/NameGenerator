ee93a5fc038dd8a236fed770f458acf819055cb8
Mahboob Ali <alimizan@hotmail.com>
2018-11-08 12:36:47 +0000

    Update nameGenerator.py

--- nameGenerator.py
+++ nameGenerator.py
@@ -1,4 +1,75 @@
+__author__ = 'Mizan'
 import string
+import random
 
-print(string.ascii_letters)
-print(string.ascii_lowercase)
+def generator():
+    letter1 = random.choice(string.ascii_lowercase)
+    letter2 =  random.choice(string.ascii_lowercase)
+    letter3 = random.choice(string.ascii_lowercase)
+    letter4 = random.choice(string.ascii_lowercase)
+    letter5 = random.choice(string.ascii_lowercase)
+
+
+
+
+
+
+    letter_input_1 = input('choose a letter V for vowels C for consonants, l for any other \n')
+    letter_input_2 = input('choose a letter V for vowels C for consonants, l for any other \n')
+    letter_input_3 = input('choose a letter V for vowels C for consonants, l for any other \n')
+    letter_input_4 = input('choose a letter V for vowels C for consonants, l for any other \n')
+    letter_input_5 = input('choose a letter V for vowels C for consonants, l for any other \n')
+
+    vowels = 'aeiouy'
+    consonants = 'bcdfghjklmnpqrstvwxz'
+    letter = string.ascii_lowercase
+
+    if letter_input_1 == "v":
+        letter1 = random.choice(vowels)
+    elif letter_input_1 == "c":
+        letter1 = random.choice(consonants)
+    elif letter_input_1 == "l":
+        letter1 = random.choice(letter)
+    else:
+        letter1 = letter_input_1
+
+    if letter_input_2 == "v":
+        letter2 = random.choice(vowels)
+    elif letter_input_2 == "c":
+        letter2 = random.choice(consonants)
+    elif letter_input_2 == "l":
+        letter2 = random.choice(letter)
+    else:
+        letter2 = letter_input_2
+
+    if letter_input_3 == "v":
+        letter3 = random.choice(vowels)
+    elif letter_input_3 == "c":
+        letter3 = random.choice(consonants)
+    elif letter_input_3 == "l":
+        letter3 = random.choice(letter)
+    else:
+        letter3 = letter_input_3
+
+    if letter_input_4 == "v":
+        letter4 = random.choice(vowels)
+    elif letter_input_4 == "c":
+        letter4 = random.choice(consonants)
+    elif letter_input_4 == "l":
+        letter4 = random.choice(letter)
+    else:
+        letter4 = letter_input_4
+
+    if letter_input_5 == "v":
+        letter5 = random.choice(vowels)
+    elif letter_input_5 == "c":
+        letter5 = random.choice(consonants)
+    elif letter_input_5 == "l":
+        letter5 = random.choice(letter)
+    else:
+        letter5 = letter_input_5
+
+    name = letter1+letter2+letter3+letter4+letter5
+    return(name)
+
+print(generator())
